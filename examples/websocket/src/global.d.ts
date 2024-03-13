f10ff104c61ea140fbc97b7fc502cfb8566bb266
Davide <43080019+edivados@users.noreply.github.com>
2024-03-13 20:05:19 +0000

    update deps and remove tauri

--- examples/websocket/src/global.d.ts
+++ examples/websocket/src/global.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="@solidjs/start/env" />
+
+import { Server } from "ws";
+
+declare global {
+  var ws: Server;
+}
+
+declare module "@solidjs/start/server" {
+  interface RequestEventLocals {
+    ws: Server
+  }
+}
--- examples/websocket/src/middleware.ts
+++ examples/websocket/src/middleware.ts
@@ -1,42 +1,55 @@
-import { eventHandler, createMiddleware, lazyEventHandler, H3Event } from "@solidjs/start/server";
+import { RequestMiddleware, createMiddleware } from "@solidjs/start/middleware";
 import { WebSocketServer } from "ws";
+
+function lazyRequestMiddleware(fn: (() => RequestMiddleware)) {
+  return fn();
+}
+
+function createWebSocketServer() {
+  if (!globalThis.ws) {
+    console.log("create websocket server");
+
+    const server: WebSocketServer = new WebSocketServer({ noServer: true, path: "/ws" });
+    globalThis.ws = server;
+    
+    server.on("connection", (socket) => {
+      console.log("client connected 🎉");
+
+      socket.on("message", (data) => {
+        server.clients.forEach(client => client != socket && client.send(data.toString("utf-8")));
+      });
+    
+      socket.on('close', () => {
+        console.log("client disconnected 👋");
+      });
+    });
+  }
+  console.log("returning websocket server");
+  return globalThis.ws;
+}
 
 export default createMiddleware({
   onRequest: [
-    lazyEventHandler(() => {
-      console.log("lazy init handler");
-    
-      const server: WebSocketServer = new WebSocketServer({ noServer: true, path: "/ws" });
-      
-      server.on("connection", (socket) => {
-        console.log("client connected 🎉");
-
-        socket.on("message", (data) => {
-          server.clients.forEach(client => client != socket && client.send(data.toString("utf-8")));
-        });
-      
-        socket.on('close', () => {
-          console.log("client disconnected 👋");
-        });
-      });
-    
-      return eventHandler(async (event) => {
+    lazyRequestMiddleware(() => {
+      console.log("lazy request middleware");
+      const server = createWebSocketServer();
+      return (event) => {
         event.locals.ws = server;
-        console.log("hitting middleware");
-        if (event.node.req.url == "/ws") {
-          console.log("hitting /ws endpoint");
-          if (event.node.req.headers["upgrade"] === "websocket") {
-            if (server.shouldHandle(event.node.req)) {
-              server.handleUpgrade(event.node.req, event.node.req.socket, Buffer.alloc(0), (client, request) => {
-                console.log("upgrade done");
-                server.emit('connection', client, request);
-              });
-              // event.node.res.writeHead(101);
-              event[Object.getOwnPropertySymbols(event)[0]]._handled = true;
+        console.log("request middleware");
+        if (server.shouldHandle(event.nativeEvent.node.req)) {
+          console.log("handle upgrade");
+          server.handleUpgrade(
+            event.nativeEvent.node.req, 
+            event.nativeEvent.node.req.socket, 
+            Buffer.alloc(0), 
+            (client, request) => {
+              console.log("upgrade done");
+              server.emit('connection', client, request);
             }
-          }
+          );
+          event.nativeEvent._handled = true;
         }
-      });
+      }
     })
   ]
 });
